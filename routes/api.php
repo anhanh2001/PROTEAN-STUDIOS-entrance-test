2970e44bb31c0975cbf8b5abe61520d04f5000a2
Phan Anh <aviet434@gmail.com>
2022-01-24 14:37:00 +0700

    login,logout,info and user route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/user',[UserController::class,'index'])->middleware('auth:sanctum');
+Route::get('/info',[UserController::class,'show'])->middleware('auth:sanctum');
+Route::post('user/{id}',[UserController::class,'update'])->middleware('auth:sanctum');
+Route::post('login',[LoginController::class,'login']);
+Route::get('logout',[LoginController::class,'logout'])->middleware('auth:sanctum');
